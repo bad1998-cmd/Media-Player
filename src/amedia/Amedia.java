c04c303fda281e76f578334e521e8c2e97f5ce1e
deepuinteriordesign <deepuinteriordesign@gmail.com>
2021-07-30 04:12:31 +0530

    Initial Commit

---
+++ src/amedia/Amedia.java
@@ -0,0 +1,49 @@
+
+package amedia;
+
+//import java.awt.event.MouseEvent;
+import javafx.application.Application;
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+
+
+/**
+ *
+ * @author gupta
+ */
+public class Amedia extends Application {
+    
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
+        
+        Scene scene = new Scene(root);
+        stage.setTitle("Amedia");
+        
+        scene.setOnMouseClicked(new EventHandler <MouseEvent> (){
+            @Override
+            public void handle(MouseEvent doubleClicked) {
+                if(doubleClicked.getClickCount()==2){
+                    stage.setFullScreen(true);
+                }
+            }
+            
+        });
+   
+        
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
